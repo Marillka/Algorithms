b676f38a1402d83fa0c0ab2058c352a1c6d7304c
Marillka <valeryrayumov@yandex.ru>
2022-04-30 15:11:04 +0300

    homework 5

---
+++ src/main/java/lesson5_Рекурсия/HM5/ExponentiationWithRecursion.java
@@ -0,0 +1,21 @@
+package lesson5_Рекурсия.HM5;
+
+public class ExponentiationWithRecursion {
+
+    public static void main(String[] args) {
+        System.out.println(exponentiation(14,4));
+    }
+
+    public static double exponentiation(double number, double degree) {
+        // Если нулевая степень
+        if (degree == 0) {
+            return 1;
+        }
+        // Если степень равна 1
+        if (degree == 1) {
+            return number;
+        }
+        degree--;
+        return number * exponentiation(number, degree);
+    }
+}
